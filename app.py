0f508de657af36724c325dce2a15262c53604a4c
rosco726 <architecte.a.racem@gmail.com>
2025-06-26 23:36:22 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ st.title("Prédiction pour le projet : aviator")
 
 # Initialisation de session_state
 if "valeurs" not in st.session_state:
-st.session_state.valeurs = []
+	st.session_state.valeurs = []
 
 # Création d'un champ de texte pour entrer une valeur
 valeur = st.text_input("Entrez la valeur 1/30", "")
 
 # Bouton pour ajouter la valeur à la liste
 if st.button("Ajoutez cette valeur"):
-try:
-val = float(valeur.replace(",", ".")) # Remplace virgule par point
-st.session_state.valeurs.append(val)
-st.success(f"Valeur {val} ajoutée.")
-except ValueError:
-st.error("Veuillez entrer une valeur numérique valide.")
+	try:
+	val = float(valeur.replace(",", ".")) # Remplace virgule par point
+	st.session_state.valeurs.append(val)
+	st.success(f"Valeur {val} ajoutée.")
+	except ValueError:
+	st.error("Veuillez entrer une valeur numérique valide.")
 
 # Affichage des valeurs entrées
 if st.session_state.valeurs:
-st.write("Valeurs enregistrées :")
-st.write(st.session_state.valeurs)
+	st.write("Valeurs enregistrées :")
+	st.write(st.session_state.valeurs)
 
 # ✅ Bouton pour réinitialiser la liste
 if st.button("Réinitialiser les valeurs"):
-st.session_state.valeurs = []
-st.success("Liste réinitialisée.")
+	st.session_state.valeurs = []
+	st.success("Liste réinitialisée.")
