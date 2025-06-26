a5c812664a03a466294cfde09b0a418e90946055
rosco726 <architecte.a.racem@gmail.com>
2025-06-26 01:03:56 +0100

    Add files via upload
    fix that app

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ valeur = st.text_input("Entrez la valeur 1/30", "")
 # Bouton pour ajouter la valeur à la liste
 if st.button("Ajoutez cette valeur"):
 try:
-val = float(valeur.replace(",", "."))
+val = float(valeur.replace(",", ".")) # Remplace virgule par point
 st.session_state.valeurs.append(val)
 st.success(f"Valeur {val} ajoutée.")
 except ValueError:
